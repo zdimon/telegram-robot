b8e2535bb79f28a75f6f9bb52d8de08d6c57ede5
zdimon <zdimon77@gmail.com>
2019-11-06 18:55:09 +0200

    auto commit

---
+++ tr/tr/local.py
@@ -0,0 +1,28 @@
+#API_ID = 1049009
+#SECRET_KEY = '7414e0395f822ad856db9cfc5ec95fbc'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tr',
+        'USER': 'postgres',
+        'PASSWORD': '1q2w3e',
+        'HOST': 'localhost',
+    }
+}
+
+
+API_ID = 314338
+SECRET_KEY = 'dfdae44a7ccfa24eed4326d54c1e31a5'
+
+'''
+1049009
+7414e0395f822ad856db9cfc5ec95fbc
+
+
+my 
+
+API_ID = 314338
+SECRET_KEY = 'dfdae44a7ccfa24eed4326d54c1e31a5'
+
+'''
--- tr/tr/settings.py
+++ tr/tr/settings.py
@@ -121,20 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-API_ID = 1049009
-SECRET_KEY = '7414e0395f822ad856db9cfc5ec95fbc'
-
-API_ID = 314338
-SECRET_KEY = 'dfdae44a7ccfa24eed4326d54c1e31a5'
-
-'''
-1049009
-7414e0395f822ad856db9cfc5ec95fbc
-
-
-my 
-
-API_ID = 314338
-SECRET_KEY = 'dfdae44a7ccfa24eed4326d54c1e31a5'
-
-'''
+from .local import *
