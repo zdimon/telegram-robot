8900c5de1ecda789064eb5ee6ec7635750a88ec8
zdimon <zdimon77@gmail.com>
2019-11-08 11:16:43 +0200

    auto commit

--- tr/main/admin.py
+++ tr/main/admin.py
@@ -8,11 +8,11 @@ from tr.settings import API_ID, SECRET_KEY, SESSION_NAME, SESSION_ROOM_NAME
 from main.models import Room
 from telethon import TelegramClient
 
-
+client = TelegramClient(SESSION_ROOM_NAME,API_ID,SECRET_KEY)
+client.start()
 
 def import_channels():
-    client = TelegramClient(SESSION_ROOM_NAME,API_ID,SECRET_KEY)
-    client.start()
+
     for room in client.iter_dialogs():
         #print(room)
         try:
