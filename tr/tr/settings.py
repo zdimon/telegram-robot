740819bff9c7ac4824808dac1be7edc37c6afafa
zdimon <zdimon77@gmail.com>
2019-11-08 11:04:43 +0200

    auto commit

--- tr/tr/settings.py
+++ tr/tr/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'tr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
