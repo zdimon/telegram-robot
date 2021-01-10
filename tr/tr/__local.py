7dbf8143b5982235fe72e64840d0c5f5ad2ff0e5
Your Name <you@example.com>
2021-01-10 13:24:56 +0300

    fix

--- tr/tr/__local.py
+++ tr/tr/__local.py
@@ -6,23 +6,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tr',
         'USER': 'postgres',
-        'PASSWORD': '1q2w3e',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
 
 
-API_ID = 314338
-SECRET_KEY = 'dfdae44a7ccfa24eed4326d54c1e31a5'
+API_ID = 345
+SECRET_KEY = '...'
 
-'''
-1049009
-7414e0395f822ad856db9cfc5ec95fbc
-
-
-my 
-
-API_ID = 314338
-SECRET_KEY = 'dfdae44a7ccfa24eed4326d54c1e31a5'
-
-'''
